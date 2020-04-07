7b4941fd6e1923623f0ebdcbd8271890624a489b
Aleksandr Kuzmenko <alex@stablex.ru>
2020-04-07 12:24:04 +0300

    wip

--- src/filters/renameVars.ml
+++ src/filters/renameVars.ml
@@ -21,11 +21,14 @@ end = struct
 	}
 
 	type scope = {
-		mutable declares : (tvar * bool) list; (* bool for in_loop *)
+		mutable declares : tvar list; (* bool for in_loop *)
 		(* key var - a declared variable, value vars - list of vars used after the key var declaration *)
-		mutable overlaps : (int, tvar list) Hashtbl.t;
-		mutable captures : (int, tvar) Hashtbl.t;
-		mutable parent : scope option;
+		overlaps : (int, tvar list) Hashtbl.t;
+		captures : (int, tvar) Hashtbl.t;
+		mutable loop_count : int;
+		(* variables referenced in current loop *)
+		loop_vars : tvar list ref;
+		parent : scope option;
 		mutable children : scope list;
 	}
 
@@ -72,6 +75,8 @@ end = struct
 		overlaps = Hashtbl.create 10;
 		captures = Hashtbl.create 5;
 		parent = parent;
+		loop_count = Option.map_default (fun p -> p.loop_count) 0 parent;
+		loop_vars = Option.map_default (fun p -> p.loop_vars) (ref []) parent;
 		children = [];
 	}
 
@@ -92,12 +97,16 @@ end = struct
 		| TAbstract (a,_) -> maybe_reserve_module_type_name r (TAbstractDecl a)
 		| TMono _ | TLazy _ | TAnon _ | TDynamic _ | TFun _ -> ()
 
-	let rename r v overlaps =
+	let rename r v scope overlaps =
 		let name = ref v.v_name in
 		let count = ref (try Hashtbl.find r.name_counters v.v_name with Not_found -> 1) in
 		let rec step() =
 			name := v.v_name ^ (string_of_int !count);
-			if StringMap.mem !name r.reserved_names || List.exists (fun o -> o.v_name = !name) overlaps then begin
+			if
+				StringMap.mem !name r.reserved_names
+				|| List.exists (fun o -> o.v_name = !name) overlaps
+				|| List.exists (fun o -> o != v && o.v_name = !name) !(scope.loop_vars)
+			then begin
 				incr count;
 				step()
 			end
@@ -110,29 +119,48 @@ end = struct
 
 	let trailing_numbers = Str.regexp "[0-9]+$"
 
-	let declare_var r scope in_loop v =
+	let declare_var r scope v =
 		(* chop escape char for all local variables generated *)
 		if is_gen_local v then begin
 			let name = String.sub v.v_name 1 (String.length v.v_name - 1) in
 			v.v_name <- "_g" ^ (Str.replace_first trailing_numbers "" name)
 		end;
-		scope.declares <- (v, in_loop) :: scope.declares;
+		scope.declares <- v :: scope.declares;
 		Hashtbl.add scope.overlaps v.v_id [];
-		if StringMap.mem v.v_name r.reserved_names then
-			rename r v []
+		if scope.loop_count > 0 then
+			scope.loop_vars := v :: !(scope.loop_vars);
+		if
+			StringMap.mem v.v_name r.reserved_names
+			|| (List.exists (fun o -> o != v && o.v_name = v.v_name) !(scope.loop_vars))
+		then
+			rename r v scope [];
+		if v.v_capture then
+			reserve_name r v.v_name
 
-	let rec reference_var r scope in_loop v =
-		let rec loop declares passed_v_declaration =
+	let add_overlap scope v o =
+		let overlaps =
+			let rec loop scope =
+				try
+					Hashtbl.find scope.overlaps o.v_id
+				with Not_found ->
+					match scope.parent with
+					| None -> assert false
+					| Some p -> loop p
+			in
+			loop scope
+		in
+		if not (List.mem v overlaps) then
+			Hashtbl.replace scope.overlaps o.v_id (v :: overlaps)
+
+	let rec reference_var r scope v =
+		let rec loop declares =
 			match declares with
 			| [] ->
 				(match scope.parent with
-				| Some parent -> reference_var r parent in_loop v
+				| Some parent -> reference_var r parent v
 				| None -> ())
-			| (d,d_loop) :: rest when d == v ->
-				if r.cfg.sc_shadowing = FullShadowing || not in_loop then ()
-				else loop rest true
-			| (_,false) :: _ when in_loop && passed_v_declaration -> ()
-			| (d,_) :: rest ->
+			| d :: rest when d == v -> ()
+			| d :: rest ->
 				let overlaps =
 					let overlaps =
 						try Hashtbl.find scope.overlaps d.v_id with Not_found -> assert false
@@ -145,10 +173,14 @@ end = struct
 						new_overlaps
 					end
 				in
-				if d.v_name = v.v_name then rename r d overlaps;
-				loop rest passed_v_declaration
+				if d.v_name = v.v_name then rename r d scope overlaps;
+				loop rest
 		in
-		loop scope.declares false
+		loop scope.declares;
+		if scope.loop_count > 0 && not (List.mem v !(scope.loop_vars)) then begin
+			List.iter (fun v2 -> add_overlap scope v2 v) !(scope.loop_vars);
+			scope.loop_vars := v :: !(scope.loop_vars)
+		end
 
 	let rec diff_declared new_declared old_declared =
 		match new_declared, old_declared with
@@ -157,45 +189,49 @@ end = struct
 		| n :: _, o :: _ when n == o -> []
 		| n :: rest_new, _ -> n :: (diff_declared rest_new old_declared)
 
-	let rec collect_vars r scope in_loop e =
+	let rec collect_vars r scope e =
 		match e.eexpr with
 		| TLocal v ->
-			reference_var r scope in_loop v
+			reference_var r scope v
 		| TVar (v, e_opt) ->
-			declare_var r scope in_loop v;
-			Option.may (collect_vars r scope in_loop) e_opt
+			declare_var r scope v;
+			Option.may (collect_vars r scope) e_opt
 		| TBlock el when r.cfg.sc_vars = BlockScope ->
 			let child = create_child_scope scope in
-			List.iter (collect_vars r child in_loop) el
+			List.iter (collect_vars r child) el
 		| TWhile (condition,body,NormalWhile) ->
-			collect_vars r scope in_loop condition;
+			scope.loop_count <- scope.loop_count + 1;
+			collect_vars r scope condition;
 			let loop_scope =
 				match r.cfg.sc_vars with
 				| BlockScope -> create_child_scope scope
 				| FunctionScope -> scope
 			in
-			collect_vars r loop_scope true body;
+			collect_vars r loop_scope body;
+			scope.loop_count <- scope.loop_count - 1;
+			if scope.loop_count = 0 then
+				scope.loop_vars := []
 		| TFor _ | TWhile (_, _, DoWhile) ->
 			(* At this point all `for` and `do` loops are expected to be transformed to `while` loops *)
 			assert false
 		| TFunction fn ->
 			let child = create_child_scope scope in
-			List.iter (fun (v,_) -> declare_var r child in_loop v) fn.tf_args;
-			List.iter (fun (v,_) -> reference_var r child in_loop v) fn.tf_args;
-			collect_vars r child false fn.tf_expr
+			List.iter (fun (v,_) -> declare_var r child v) fn.tf_args;
+			List.iter (fun (v,_) -> reference_var r child v) fn.tf_args;
+			collect_vars r child fn.tf_expr
 		| TTry (try_body, catches) ->
-			collect_vars r scope in_loop try_body;
+			collect_vars r scope try_body;
 			List.iter (fun (v, catch_body) ->
 				let scope =
 					match r.cfg.sc_vars with
 					| BlockScope -> create_child_scope scope
 					| FunctionScope -> scope
 				in
-				declare_var r scope in_loop v;
-				collect_vars r scope in_loop catch_body
+				declare_var r scope v;
+				collect_vars r scope catch_body
 			) catches
 		| _ ->
-			iter (collect_vars r scope in_loop) e
+			iter (collect_vars r scope) e
 
 	let run r ctx e =
 		let r = { r with name_counters = Hashtbl.copy r.name_counters } in
@@ -204,7 +240,7 @@ end = struct
 			| s :: _,_ | [],s -> reserve_name r s
 		end;
 		let root_scope = create_scope None in
-		collect_vars r root_scope false e;
+		collect_vars r root_scope e;
 		(* let rec loop scope =
 			List.iter (fun v ->
 				maybe_rename r v;
